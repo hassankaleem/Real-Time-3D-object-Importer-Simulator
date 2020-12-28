a4fa44222acf6e2b8d9647cad7c826d8ac856327
hassankaleem <ranahassankaleem99@yahoo.com>
2020-12-28 18:23:08 +0500

    c32
    c32

---
+++ Assets/openfolder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//
+//public class openfolder : MonoBehaviour {
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+//}
+using System.IO;
+using UnityEngine;
+//using UnityEditor;
+
+public class openfolder : MonoBehaviour
+{
+//	[MenuItem("Example/Overwrite Texture")]
+//	static void Apply()
+//	{
+//		Texture2D texture = Selection.activeObject as Texture2D;
+//		if (texture == null)
+//		{
+//			EditorUtility.DisplayDialog("Select Texture", "You must select a texture first!", "OK");
+//			return;
+//		}
+//		string path = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
+//		if (path.Length != 0)
+//		{
+//			var fileContent = File.ReadAllBytes(path);
+//			texture.LoadImage(fileContent);
+//		}
+//	}
+}
---
+++ Assets/pickit.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class pickit : MonoBehaviour {
+
+	private Transform taker;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown("t")) {
+
+			transform.parent = taker;
+
+		} else if(Input.GetKeyDown("g")) {
+
+			transform.parent = null;
+
+		}
+
+	}
+
+	void OnTriggerEnter (Collider col) {
+
+		if(col.CompareTag("Player")) {
+			Debug.Log ("collosin enter");
+			taker = col.transform;
+
+		}
+
+	}    
+
+	void OnTriggerExit (Collider col) {
+
+		if(col.CompareTag("Player")) {
+			taker = null;
+		}
+
+	}
+}
